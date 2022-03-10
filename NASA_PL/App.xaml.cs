712ae2aad32a83583a9bc8630b13b45dc32c4043
Ron Keinan <ronke21@gmail.com>
2022-03-10 22:22:59 +0200

    trying to drag

--- NASA_PL/App.xaml.cs
+++ NASA_PL/App.xaml.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Windows;
+
+using System.Windows.Controls;
+
+using System.Windows.Data;
+
+using System.Windows.Documents;
+
+using System.Windows.Input;
+
+using System.Windows.Media;
+
+using System.Windows.Media.Imaging;
+
+using System.Windows.Navigation;
+
+using System.Windows.Shapes;
 
 namespace NASA_PL
 {
@@ -13,5 +41,12 @@ namespace NASA_PL
     /// </summary>
     public partial class App : Application
     {
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+            }
+        }
     }
 }
