503f31de6565b3e56ec2328b3567903ec0899aef
AmihayHassan <amihassan2@gmail.com>
2022-03-13 16:26:59 +0200

    UI

--- NASA_PL/Views/MainWindowView.xaml.cs
+++ NASA_PL/Views/MainWindowView.xaml.cs
@@ -31,8 +31,6 @@ namespace NASA_PL.Views
         public MainWindowView()
         {
             InitializeComponent();
-
-
         }
 
         #region mouse effects and functionality for exit button
@@ -84,30 +82,8 @@ namespace NASA_PL.Views
 
         #endregion
 
-        #region Buses button
-        private void BusesSidePanel_OnMouseEnter(object sender, RoutedEventArgs e)
-        {
-            var bc = new BrushConverter();
-            BusesSidePanel.Background = (Brush)bc.ConvertFrom("#30ABFF");
-        }
-        private void BusesSidePanel_OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            BusesSidePanel.Background = (Brush)bc.ConvertFrom("#3d1865");
-        }
-        private void BusesSidePanel_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (!_hidden)
-            {
-                ButtonBase_OnClick(sender, e);
-            }
 
-            //if (!(DataDisplay.Content is BusesViewPage))
-            //{
-            //    DataDisplay.Content = new BusesViewPage(_bl);
-            //}
-        }
-        #endregion
+
 
         #region Stations button
         private void StationsSidePanel_OnClick(object sender, RoutedEventArgs e)
@@ -122,21 +98,6 @@ namespace NASA_PL.Views
             //    DataDisplay.Content = new StationsViewPage(_bl, _simulationPage);
             //}
         }
-
-        #region colors
-        private void StationsSidePanel_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            StationsSidePanel.Background = (Brush)bc.ConvertFrom("#30ABFF");
-        }
-
-        private void StationsSidePanel_OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            StationsSidePanel.Background = (Brush)bc.ConvertFrom("#3d1865");
-        }
-
-        #endregion
         #endregion
 
         #region Lines button
@@ -151,16 +112,6 @@ namespace NASA_PL.Views
             //{
             //    DataDisplay.Content = new LinesViewPage(_bl);
             //}
-        }
-        private void LinesSidePanel_OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            LinesSidePanel.Background = (Brush)bc.ConvertFrom("#3d1865");
-        }
-        private void LinesSidePanel_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            LinesSidePanel.Background = (Brush)bc.ConvertFrom("#30ABFF");
         }
         #endregion
 
@@ -178,29 +129,9 @@ namespace NASA_PL.Views
             //}
 
         }
-        private void ConsecutiveStationsSidePanel_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            ConsecutiveStationsSidePanel.Background = (Brush)bc.ConvertFrom("#30ABFF");
-        }
-        private void ConsecutiveStationsSidePanel_OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            ConsecutiveStationsSidePanel.Background = (Brush)bc.ConvertFrom("#3d1865");
-        }
         #endregion
 
         #region Simulator
-        private void SimulatorSidePanel_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            SimulatorSidePanel.Background = (Brush)bc.ConvertFrom("#30ABFF");
-        }
-        private void SimulatorSidePanel_OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            var bc = new BrushConverter();
-            SimulatorSidePanel.Background = (Brush)bc.ConvertFrom("#3d1865");
-        }
         private void SimulatorSidePanel_OnClick(object sender, RoutedEventArgs e)
         {
             if (!_hidden)
